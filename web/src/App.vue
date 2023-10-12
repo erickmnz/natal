ffaf9b90ea13f182896744cc88d38cc1350302d1
erickmnz <ericklmenezes@outlook.com>
2023-10-12 20:03:56 -0300

    fix async

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,32 +7,34 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
+
+
+
     
-  }, data() {
-    return {resp:""}
-      
-  },methods:{
-    getNatal(){
   
-      const response:Promise<Response> = fetch("http://localhost:2345/api/natal"
-);
-        response
-        .then((r:Response)=>{return r.json()})
-        .then((c:any)=>{
-          this.resp=c?.natal ? "Sim, é natal.":"Ainda não"
-        })
+    const resp = ref("")
+
+    async function getNatal(){
+      try{
+        
+      const response= await fetch("http://localhost:2345/api/natal"
+      );
+
+      const jsons = await response.json()
+      resp.value = jsons.natal ? "Sim, é natal.":"Ainda não"
+       
+      }catch(err){
+        console.log("Error: ", err)
+      }
         
         
       
       
     }
-  }
-});
+  
+;
 </script>
 
