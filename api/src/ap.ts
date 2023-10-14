15cc492682adef7be7346b702b24b9d6d3af8569
erickmnz <ericklmenezes@outlook.com>
2023-10-14 13:37:49 -0300

    Refactoring

--- api/src/ap.ts
+++ api/src/ap.ts
@@ -4,7 +4,6 @@ export const app= express();
 
 app.get("/api/natal",(req,res)=>{
     let date=new Date();
-    console.log(req)
     res.send({
          "natal":`12/25`===`${date.getMonth()}/${date.getDay()}`
     })
--- api/src/index.ts
+++ api/src/index.ts
@@ -2,4 +2,4 @@ import { app } from "./ap";
 
 const port = 2345;
 
-app.listen(port, ()=>{console.log("zzz")}) 
+app.listen(port) 
